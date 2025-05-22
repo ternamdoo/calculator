592e5b2c8d678aa8eb5a62de75405cf80298d842
Ternamdoo <jerryaliyese@gmail.com>
2025-05-22 16:43:05 +0100

    Define the functions for the calculator operations

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // Put a placeholder 0 on the display
 const display = document.querySelector('.display');
 display.textContent = "0";
+
+// Define the functions for the calculator operator keys
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b;
+}
