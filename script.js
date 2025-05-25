600432aaeb0180ed4171d62de952640b0a67b1d8
Ternamdoo <jerryaliyese@gmail.com>
2025-05-25 16:27:55 +0100

    Show clicked numbers on display

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ let firstNumber;
 let operator;
 let secondNumber;
 
+// Get the digits when clicked
+let clickedNumber;
+const digits = document.querySelectorAll('.number');
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        clickedNumber = digit.textContent;
+        if (display.textContent === '0') {
+            display.textContent = clickedNumber;
+        } else {
+            display.textContent += clickedNumber;
+        }
+    })
+});
+
 // Define the functions for the calculator operator keys
 const add = function(a, b) {
     return a + b;
