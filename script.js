fbb8995fdb2d57ed377f6d2737b4c79f88a6771f
Ternamdoo <jerryaliyese@gmail.com>
2025-05-26 20:20:52 +0100

    Clean up the code

--- script.js
+++ script.js
@@ -82,20 +82,12 @@ const deleteLastInput = function(str) {
     return str.slice(0, (str.length -1));
 }
 
-// I'm building a calculator app using JavaScript. 
-// The user may click a button on the calculator and it'll appear on the display. 
-// However, I don't know how to implement the following logic:
-
-// - The user enters a sequence of numbers
-// - If the user clicks any operator button (+, -, *, /), 
-// The operator type is saved to another variable operator.
+// Get operator
 const arithmeticOperators = document.querySelectorAll('.arithmetic');
 arithmeticOperators.forEach((button) => {
     button.addEventListener('click', () => {
-        // the first sequence of numbers is saved to a variable, firstNumber. 
         firstNumber = Number(display.textContent);
         display.textContent = '0';
-        alert(firstNumber);
         switch (button.textContent) {
             case '/':
                 operator = divide;
@@ -110,25 +102,16 @@ arithmeticOperators.forEach((button) => {
                 operator = subtract;
                 break;
         }
-        alert(operator);
     })
 });
 
-// - The user may then enter another sequence of numbers
 
-// - If the user clicks on the = sign, the  new number sequence is saved to 
-// a new variable, num2. An operate function is then called. 
-// The operate function takes 3 arguments, num1, num2, and the operator. 
-// The operator is a function that takes num1 and num2 and performs the 
-// operation on both numbers and returns a result.
+// Evalluate an expression
 const evaluate = document.querySelector('.equals');
 evaluate.addEventListener('click', () => {
     secondNumber = Number(display.textContent);
-    alert(secondNumber);
     const result = operate(firstNumber, secondNumber, operator);
     display.textContent = result;
-    alert(result);
-    // display.textContent = result;
 })
 
 // - If the user clicks on another operator after clicking on the second 
